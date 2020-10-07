97757c72d62409400b5a3e9c9730b50ecb34b3fe
Benjamin Steidl <localartpool@gmail.com>
2020-10-07 14:45:24 +0200

    Item Creation and Prisma Yoga flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,10 +1,15 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    //Create a dog!
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    //TODO: Check if there are looged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return item;
   },
 };
 
