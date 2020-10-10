d7cfbc9ca7f82111accb39a96ac4aed836e59497
Benjamin Steidl <localartpool@gmail.com>
2020-10-10 14:53:08 +0200

    Worked on image upload

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
+import Router from 'next/router';
 import gql from 'graphql-tag';
 import Form from './styles/Form'; 
 import formatMoney from '../lib/formatMoney';
+import Error from './ErrorMessage';
 
 const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
@@ -37,16 +39,58 @@ class CreateItem extends Component {
     const val = type === 'number' ? parseFloat(value) : value;
     this.setState({ [name]: val });
   };
+
+  uploadFile = async e => {
+    console.log('uploading file...');
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sick-fits'); 
+
+    const res = await fetch
+    ('https://api.cloudinary.com/v1_1/daf8fgekl/image/upload', {
+      method: 'POST',
+      body: data,
+    });
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
+  };
   render() {
     return(
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}> 
         {(createItem, { loading, error }) => (
           <Form 
-            onSubmit={(e) => {
-            e.preventDefault();
-            console.log(this.state);
-            }}>
-            <fieldset>
+            onSubmit={ async e => {
+              //Stop the form from submitting 
+              e.preventDefault();
+              //Call the Mutation
+              const res = await createItem();
+              //Change them to the single item page
+              console.log(res);
+              Router.push({
+                pathname: '/item',
+                query: { id: res.data.createItem.id },
+              });
+            }}
+          >
+          <Error error={error} />
+            <fieldset disabled={loading} aria-busy={loading}>
+            <label htmlFor="file">
+                Image
+                <input 
+                  type="file" 
+                  id="file" 
+                  name="file" 
+                  placeholder="Upload an image" 
+                  required 
+                  onChange={this.handleChangeuploadFile}
+                />
+              </label>
+              
               <label htmlFor="tilte">
                 Title
                 <input 
