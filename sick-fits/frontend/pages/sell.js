f50afb769926a37ea092e34d76fe5fa6df20e3fe
Benjamin Steidl <localartpool@gmail.com>
2020-10-10 12:32:45 +0200

    Creating items with mutation

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,8 +1,8 @@
-import Link from "next/link";
+import CreateItem from "../components/CreateItem";
 
 const Sell = (probs) => (
   <div>
-    <p>Sell!</p>
+    <CreateItem />
   </div>
 );
 export default Sell;
