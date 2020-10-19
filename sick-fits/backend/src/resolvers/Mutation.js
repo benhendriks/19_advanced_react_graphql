3d372df6c873f84317d42c569cd44483a40a91cf
Benjamin Steidl <localartpool@gmail.com>
2020-10-19 18:17:52 +0200

    Worked on user permissions

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -2,15 +2,24 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
-
 const { transport, makeANiceEmail } = require('../mail');
+const { hasPermission } = require('../utils');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    //TODO: Check if there are looged in
+    if(!ctx.request.userId) {
+      throw new Error('You must be logged in to do that!');
+    }
+    
     const item = await ctx.db.mutation.createItem(
       {
         data: {
+          // This how we create an relationsship between the item and the user
+          user: {
+            connect: {
+              id: ctx.request.userId
+            },
+          },
           ...args,
         },
       },
@@ -100,12 +109,12 @@ const Mutations = {
 
     //3. Email them that reset token 
     const mailRes = await transport.sendMail({
-      from: 'localartpool@gmail.com',
+      from: 'wes@wesbos.com',
       to: user.email,
       subject: 'Your Password Reset',
-      html: makeANiceEmail(`Your PasswordReset Token is here!
+      html: makeANiceEmail(`Your Password Reset Token is here!
       /n/n
-       <a href=${process.env.FRONTEND_URL}/reset?resetToken=${resetToken  }>Click here to reset!</a>`),
+       <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">Click here to reset!</a>`),
     });
 
     //4. Return the message
@@ -149,6 +158,35 @@ const Mutations = {
     //8. return the new user
     return updatedUser; 
   },
-};
+  async updatePermission(parent, args, ctx, info) {
+    //1. Check if they are logged in
+    if(!ctx.request.userId) {
+      throw new Error('You must be logged in');
+    }
+    //2. Query the current user
+    const currentUser = await ctx.db.query.user({
+      where: {
+        id: ctx.request.userId,
+      },
+    },
+    info
+    );
+    //3. Check if they have permissions to do this
+    hasPermission(currentUser, ['ADMIN', 'PERMISSIONUPDATE', '']
+    );
+    //4. Update the permissions
+    return ctx.db.mutation.updateUser({
+      data: {
+        permissions: {
+          set: args.permissions,
+        },
+      },
+      where: {
+        id: args.userId
+      },
+    }, 
+    info);
+  },
+}; 
 
 module.exports = Mutations; 
