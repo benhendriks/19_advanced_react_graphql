827ca7209d73163d2a39f90b7723e6a461cc0c35
Benjamin Steidl <localartpool@gmail.com>
2020-10-21 16:41:38 +0200

    Worked animated cart count component

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -27,22 +27,44 @@ class RemoveFromCart extends React.Component {
   static propTypes = {
     id: PropTypes.string.isRequired,
   };
+  //This gets called as soon as we get a response back from the server after our mutation has been performed
+  update = (cache, payload) => {
+    console.log('Running remove from update function');
+    //1. First read the cache 
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY});
+    console.log(data);
+    //2. remove that item from the cart 
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    //3. write it back to the cache 
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
   render() {
     return (
-      <Mutation mutation={REMOVE_FROM_CART_MUTATION} 
-      variables={{id: this.props.id}}>
-        {(removeFromCart, { loading, error }) => (
-          <BigButton 
-            disabled={loading}
-            onClick={() => {
-              removeFromCart().catch(err => alert
-              (err.message));
-            }} 
-            title="Delete Item"
-          >
-            &times;
-          </BigButton>
-        )}
+      <Mutation 
+        mutation={REMOVE_FROM_CART_MUTATION} 
+        variables={{id: this.props.id}} 
+        update={this.update}
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeFromCart: {
+            __typename: 'CartItem',
+            id: this.props.id,
+          },
+        }}
+      >
+          {(removeFromCart, { loading, error }) => (
+            <BigButton 
+              disabled={loading}
+              onClick={() => {
+                removeFromCart().catch(err => alert
+                (err.message));
+              }} 
+              title="Delete Item"
+            >
+              &times;
+            </BigButton>
+          )}
       </Mutation>
     );
   }
